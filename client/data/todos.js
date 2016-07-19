c3c803d186eff85e8621573284816350ca6bc3cb
iulia0103 <iulia.leancu@gmail.com>
2016-07-19 17:44:30 +0200

    first commit

---
+++ client/data/todos.js
@@ -0,0 +1,24 @@
+const todos = [
+  {
+    text: 'read that js book',
+    completed: false
+  },
+  {
+    text: 'make another react+redux tutorial',
+    completed: true
+  },
+  {
+    text: 'go to the monthly training',
+    completed: false
+  },
+  {
+    text: 'apply to 5 jobs',
+    completed: false
+  },
+  {
+    text: 'buy plane tickets',
+    completed: false
+  },
+];
+
+export default todos;
