4a25f8bb1d843eaf885ba5aa9a59741128b7fc88
iulia0103 <iulia.leancu@gmail.com>
2016-07-27 15:04:26 +0200

    add svg's for completed and uncompleted tasks, add a type for habits

--- client/components/Habits.js
+++ client/components/Habits.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
+import {MdClear} from 'react-icons/lib/md';
 
 const Habits = React.createClass({
   handleSubmit(event){
@@ -36,7 +37,7 @@ const Habits = React.createClass({
               <span><Button bsStyle='default' bsSize='small' onClick={this.props.increaseHealth.bind(null, i)}>&#43;</Button></span>
               <span><Button bsStyle='default' bsSize='small' onClick={this.props.decreaseHealth.bind(null, i)}>&#8722;</Button></span>
               <span>   {habit.text}   </span>
-              <button onClick={this.props.removeHabit.bind(null, i)}>&times;</button>
+              <MdClear onClick={this.props.removeHabit.bind(null, i)} />
             </p>)}
         </div>
       </div>
