ff4f14bd1027c40edd9e1f8476a1a6d4a02fd65a
Tadeusz Łazurski <tadeusz@lazurski.pl>
2016-07-28 09:33:06 +0200

    Use ADD_TASK action to fill state with initial set of tasks
    The description in data/tasks.js is stripped from all properties, except the ones expected by action creator (taskType and text).
    
    In todoapp.js, before the Provider and Router are initialized, actions are dispatch to fill the state with tasks. I leave the implementation of habits and progressbars to you.

--- client/todoapp.js
+++ client/todoapp.js
@@ -12,6 +12,14 @@ import store, {history} from './store.js';
 
 import 'bootstrap/less/bootstrap.less'
 
+import { addTask } from './actions/actionCreators'
+import tasks from './data/tasks.js';
+import habits from './data/habits.js';
+import progressbars from './data/progressbars.js';
+
+tasks.forEach(({taskType, text}) => store.dispatch(addTask(taskType, text)))
+// TODO: The same for habits and progressbars
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
