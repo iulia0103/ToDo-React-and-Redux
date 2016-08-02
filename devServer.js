790adcafeba17e5bde286b729fa8e53dacf65f6e
iulia0103 <iulia.leancu@gmail.com>
2016-08-02 15:44:32 +0200

    change port

--- devServer.js
+++ devServer.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
+var port = process.env.PORT || 7770;
 
 var app = express();
 var compiler = webpack(config);
@@ -17,7 +18,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(7770, 'localhost', function(err) {
+app.listen(port, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
